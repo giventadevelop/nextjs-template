5151230dac7a2d0255d1515bf4dcc5a435c16a49
giventadevelop <giventa.develop@gmail.com>
2025-04-26 18:22:34 -0400

    payment subs

--- next.config.mjs
+++ next.config.mjs
@@ -42,6 +42,16 @@ const nextConfig = {
   experimental: {
     // Add experimental features here
   },
+
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+    // ...add any other required env vars
+  },
 };
 
 export default nextConfig;
